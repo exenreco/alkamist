a7b9cd795233716b6666776aa672ba6678dcbb08
exenreco <exenreco19@yahoo.com>
2023-02-11 03:00:26 -0600

    New templates added
    added theme templates:
    -full-width
    -boxed
    -blank
    -wide

---
+++ patterns/content-default.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Default Content
+ * 
+ * Description: The default one column layout the alkamist theme ships with.
+ * 
+ * Slug: alkamist/content-default
+ * 
+ * Categories: alkamist, content
+ * 
+ * Keywords: alkamist, content, one column, default
+ * 
+ * Block Types: core/template-part/header
+ */
+?>
+
+<!-- wp:group {"tagName":"main","className":"alkamist_page_content","layout":{"type":"constrained"}} -->
+<main id="alkamist-page-content" class="wp-block-group alkamist_page_content">
+    <!-- wp:post-content {"align":"full"} /-->
+</main>
+<!-- /wp:group -->
